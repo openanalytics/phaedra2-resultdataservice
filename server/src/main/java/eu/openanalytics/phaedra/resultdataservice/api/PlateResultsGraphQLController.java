fa86da82f7ee81496ad896ed268ea9e611e82440
sberberovic <sasa.berberovic@openanalytics.eu>
2023-07-19 10:59:11 +0200

    update: Add logging for debugging

--- server/src/main/java/eu/openanalytics/phaedra/resultdataservice/api/PlateResultsGraphQLController.java
+++ server/src/main/java/eu/openanalytics/phaedra/resultdataservice/api/PlateResultsGraphQLController.java
@@ -31,6 +31,8 @@ import eu.openanalytics.phaedra.resultdataservice.dto.ResultSetDTO;
 import eu.openanalytics.phaedra.resultdataservice.exception.ResultSetNotFoundException;
 import eu.openanalytics.phaedra.resultdataservice.model.*;
 import eu.openanalytics.phaedra.resultdataservice.service.ResultSetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -46,6 +48,8 @@ public class PlateResultsGraphQLController {
     private final ProtocolServiceClient protocolServiceClient;
     private final PlateServiceClient plateServiceClient;
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public PlateResultsGraphQLController(ResultSetService resultSetService, ProtocolServiceClient protocolServiceClient, PlateServiceClient plateServiceClient) {
         this.resultSetService = resultSetService;
         this.protocolServiceClient = protocolServiceClient;
@@ -55,14 +59,17 @@ public class PlateResultsGraphQLController {
     @QueryMapping
     public PlateResultSetData latestPlateResultByPlateId(@Argument long plateId) throws ResultSetNotFoundException, PlateUnresolvableException, ProtocolUnresolvableException {
         ResultSetDTO resultSetDTO = resultSetService.getLatestResultSetByPlateId(plateId, Optional.empty());
+
         PlateDTO plateDTO = plateServiceClient.getPlate(plateId);
         List<WellDTO> wellDTOs = plateServiceClient.getWells(plateId);
-        ProtocolDTO protocolDTO = protocolServiceClient.getProtocol(resultSetDTO.getProtocolId());
+        logger.info(String.format("Number of wells for plate %s found: $s", plateId, wellDTOs.size()));
+        List<WellData> wells = wellDTOs.stream().map(wellDTO -> new WellData(wellDTO.getId(), wellDTO.getWellType(), wellDTO.getRow(), wellDTO.getColumn())).collect(Collectors.toList());
+        logger.info(String.format("Number of WellDTO objects mapped to WellData records: $s", wells.size()));
+        PlateData plate = new PlateData(plateId, plateDTO.getBarcode(), wells);
 
+        ProtocolDTO protocolDTO = protocolServiceClient.getProtocol(resultSetDTO.getProtocolId());
         List<FeatureData> features = protocolDTO.getFeatures().stream().map(f -> new FeatureData(f.getId(), f.getName())).collect(Collectors.toList());
         ProtocolData protocol = new ProtocolData(protocolDTO.getId(), protocolDTO.getName(), features);
-        List<WellData> wells = wellDTOs.stream().map(wellDTO -> new WellData(wellDTO.getId(), wellDTO.getWellType(), wellDTO.getRow(), wellDTO.getColumn())).collect(Collectors.toList());
-        PlateData plate = new PlateData(plateId, plateDTO.getBarcode(), wells);
 
         return new PlateResultSetData(plate, protocol);
     }
