cb01069a7bb3000690fd283fad34c2a83eb027fe
sberberovic <sasa.berberovic@openanalytics.eu>
2023-09-13 13:41:37 +0200

    fix: update ResultSetGraphQLController class annotation to @Controller

--- server/src/main/java/eu/openanalytics/phaedra/resultdataservice/api/ResultSetGraphQLController.java
+++ server/src/main/java/eu/openanalytics/phaedra/resultdataservice/api/ResultSetGraphQLController.java
@@ -30,11 +30,12 @@ import eu.openanalytics.phaedra.util.exceptionhandling.UserVisibleExceptionHandl
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Controller
 public class ResultSetGraphQLController {
 
     private final ResultSetService resultSetService;
