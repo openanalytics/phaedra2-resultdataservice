05e9eac51ac75974ace183a32a22b51dc68f3781
sberberovic <sasa.berberovic@openanalytics.eu>
2024-10-14 10:38:26 +0200

    fix: sql conversion error

--- server/src/main/java/eu/openanalytics/phaedra/resultdataservice/repository/CustomResultSetRepositoryImpl.java
+++ server/src/main/java/eu/openanalytics/phaedra/resultdataservice/repository/CustomResultSetRepositoryImpl.java
@@ -1,13 +1,14 @@
 package eu.openanalytics.phaedra.resultdataservice.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.openanalytics.phaedra.resultdataservice.model.ResultSet;
-import eu.openanalytics.phaedra.resultdataservice.model.ResultSet.ErrorHolder;
-import eu.openanalytics.phaedra.resultdataservice.model.ResultSet.StatusCodeHolder;
+import eu.openanalytics.phaedra.resultdataservice.model.ResultSet.ErrorReadingConverter;
+import eu.openanalytics.phaedra.resultdataservice.model.ResultSet.StatusCodeHolderReadingConvertor;
 import eu.openanalytics.phaedra.resultdataservice.record.ResultSetFilter;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.StringJoiner;
 import org.apache.commons.collections4.CollectionUtils;
+import org.postgresql.util.PGobject;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -50,6 +51,8 @@ public class CustomResultSetRepositoryImpl implements CustomResultSetRepository 
   private static class ResultSetMapper implements RowMapper<ResultSet> {
     @Override
     public ResultSet mapRow(java.sql.ResultSet rs, int rowNum) throws SQLException {
+      ErrorReadingConverter errorReadingConverter = new ErrorReadingConverter(new ObjectMapper());
+      StatusCodeHolderReadingConvertor statusCodeHolderReadingConvertor = new StatusCodeHolderReadingConvertor();
       ResultSet resultSet = new ResultSet()
           .withId(rs.getLong("id"))
           .withProtocolId(rs.getLong("protocol_id"))
@@ -57,9 +60,9 @@ public class CustomResultSetRepositoryImpl implements CustomResultSetRepository 
           .withMeasId(rs.getLong("meas_id"))
           .withExecutionStartTimeStamp(rs.getTimestamp("execution_start_time_stamp").toLocalDateTime())
           .withExecutionEndTimeStamp(rs.getTimestamp("execution_end_time_stamp").toLocalDateTime())
-          .withErrors(rs.getObject("errors", ErrorHolder.class))
+          .withErrors(errorReadingConverter.convert((PGobject) rs.getObject("errors")))
           .withErrorsText(rs.getString("errors_text"))
-          .withOutcome(rs.getObject("outcome", StatusCodeHolder.class));
+          .withOutcome(statusCodeHolderReadingConvertor.convert(rs.getString("outcome")));
       return resultSet;
     }
   }
